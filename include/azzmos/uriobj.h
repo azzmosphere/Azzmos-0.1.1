4fb1a9c342362c304254125bfea3deb70e6ca77b
Aaron Spiteri <aaron@azzmosphere-com-au.local>
2010-09-15 23:20:47 +1000

    Changed configuration and file locations,  also made uriob allot less bloated

--- include/azzmos/uriobj.h
+++ include/azzmos/uriobj.h
@@ -18,6 +18,7 @@
 
 
 /* #####   HEADER FILE INCLUDES   ################################################### */
+#define __AZZMOS_URIOBJ_H__
 #ifndef __AZZMOS_COMMON_H__
 #include <azzmos/common.h>
 #endif
@@ -52,24 +53,13 @@ struct uriobj_s {
 	long   uri_flags;     /* various flags for the uri */
 } typedef uriobj_t;
 
-/* #####   EXPORTED VARIABLES   ##################################################### */
-
 /* #####   EXPORTED FUNCTION DECLARATIONS   ######################################### */
-extern char      *uri_remove_dot_segments( char **);
-extern int        uri_init_regex( regexpr_t *re);
-extern int        uri_parse( uriobj_t *uri, regexpr_t *re, const char *fqp);
-extern char      *uri_merge_paths( uriobj_t *base, uriobj_t *rel);
-extern uriobj_t  *uri_trans_ref( uriobj_t *base, uriobj_t *ref, bool strict);
-extern char      *uri_comp_recomp( uriobj_t *uri);
-extern int        uri_norm_scheme( uriobj_t *uri);
-extern int        uri_norm_auth( uriobj_t *uri);
-extern int        uri_norm_host( uriobj_t *uri);
-extern int        uri_norm_port( uriobj_t *uri);
-extern int        norm_pct( char **pct);
-extern int        uri_norm_port( uriobj_t *uri);
-extern int        uri_norm_ipv4( uriobj_t *uri);
-extern int        uri_norm_ipv6( uriobj_t *uri);
-extern int        uri_norm_auth( uriobj_t *uri);
-extern int        uri_auth_sync( uriobj_t *uri);
-extern int        uri_normalize( uriobj_t *uri);
-extern int        uri_norm_ip( uriobj_t *uri);
+extern char *pop_segment( char **path);
+extern char *shift_segment( char **path, const int offset);
+extern char *get_next_segment( char **path);
+extern char *replace_prefix( char **path);
+extern void  init_uriobj_str( uriobj_t *uri);
+extern char *uri_strcpy( char **s1, const char *s2);
+extern char *uri_strcat( char *s1, const char *format, const char *s2);
+
+
--- include/azzmos/utils.h
+++ include/azzmos/utils.h
@@ -27,9 +27,10 @@
 char * usplice( const char *in, unsigned int start, unsigned int end);
 char * _macitoa_( int num );
 extern void _syslog_print_error( unsigned int tid, char *fname, int lineno, char *m1, char *m2, int pri );
-//extern void _syslog_print_error_url( unsigned int tid, char *fname, int lineno, char *m1, char *url, char *m2, int pri );
 inline void  reset_file ( FILE *fh );
 
+
+/* #####   EXPORTED MACROS   ######################################################## */
 #define P_SELF (unsigned int) pthread_self()
 #define __L_COMMON P_SELF, __FILE__, __LINE__
 #define DEBUG(m) syslog( LOG_DEBUG, "%u:%s:%d - %s",  __L_COMMON, m)
@@ -40,9 +41,6 @@ inline void  reset_file ( FILE *fh );
 #define ERROR(m) ERROR_B(m,strerror(errno))
 #define ERROR_E(m,e) ERROR_B(m, strerror(e))
 #define ERROR_F(f)  ERROR_B("could not print file - %s", f)
-//#define ERROR_URL(m1,u,m2)  _syslog_print_error_url(__L_COMMON, m1,u,m2, LOG_ERR)
-//#define ERROR_C(m,c) ERROR_B(m, (char *) curl_easy_strerror(c))
-//#define ERROR_C_URL(m, u, c) ERROR_URL(m, (char *) u, (char *) curl_easy_strerror(c))
 #define WARN_F(f,e)   syslog( LOG_WARNING, "%u:%s:%d - %s reading - %s - %s",__L_COMMON, f, e)
 #define WARN(m)       syslog( LOG_WARNING, "%u:%s:%d - %s", __L_COMMON, m)
 #define WARN_E(m,e)   syslog( LOG_WARNING, "%u:%s:%d - %s - %s", __L_COMMON, m, strerror(e))
@@ -50,6 +48,17 @@ inline void  reset_file ( FILE *fh );
 #define CRIT_B(m1,m2) _syslog_print_error(__L_COMMON, m1,m2, LOG_CRIT) 
 #define CRIT(m) CRIT_B(m, strerror(errno))
 #define CRIT_E(m, e) CRIT_B(m, strerror(e))
+
+/**************************************************************************************
+ * The following functions are stop the program from breaking by trying to malloc NULL
+ **************************************************************************************/
+#define URI_CP_PT(p) (*p)?strdup(*p):NULL
+#define UI(u) ((*u)?*u:"")
+
+//extern void _syslog_print_error_url( unsigned int tid, char *fname, int lineno, char *m1, char *url, char *m2, int pri );
+//#define ERROR_URL(m1,u,m2)  _syslog_print_error_url(__L_COMMON, m1,u,m2, LOG_ERR)
+//#define ERROR_C(m,c) ERROR_B(m, (char *) curl_easy_strerror(c))
+//#define ERROR_C_URL(m, u, c) ERROR_URL(m, (char *) u, (char *) curl_easy_strerror(c))
 //#define CRIT_C(m,c) CRIT_B(m, (char *) curl_easy_strerror(c))
 
 
